316880f14d9a907ffe5b5e8d86cc688de2e58a31
nicole-gathany <nicole.gathany@gmail.com>
2021-05-14 23:08:29 -0400

    includes

---
+++ includes/examples/includes_two/index.js
@@ -0,0 +1,9 @@
+let includesTwo = function(array){
+    return array.includes(2)
+}
+
+array1 = [1, 2, 3, 4, 5];
+array2 = [0, 0, 0];
+
+console.log(includesTwo(array1));
+console.log(includesTwo(array2));
