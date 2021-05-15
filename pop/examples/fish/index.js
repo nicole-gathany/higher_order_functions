36f16f7809da9365811fe1a7de73f812a986d5ff
nicole-gathany <nicole.gathany@gmail.com>
2021-05-14 23:38:46 -0400

    all pop

---
+++ pop/examples/fish/index.js
@@ -0,0 +1,7 @@
+var myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+var popped = myFish.pop();
+
+console.log(myFish); // ['angel', 'clown', 'mandarin' ]
+
+console.log(popped); // 'sturgeon
---
+++ pop/examples/numbers/index.js
@@ -0,0 +1,2 @@
+var numbers = [1, 4, 9];
+var element = numbers.pop();
