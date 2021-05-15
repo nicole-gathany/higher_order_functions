a434c7acba5e1dcac44b4f84e11fef9e75f134b5
nicole-gathany <nicole.gathany@gmail.com>
2021-05-14 22:36:51 -0400

    added instructions

--- reduce/examples/even_last/index.js
+++ reduce/examples/even_last/index.js
@@ -1,3 +1,9 @@
+// Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+// Indices in sequence start from 0.
+
+// If the sequence is empty, you should return 0.
+
 function evenLast(numbers) {
     // Good luck
     if (numbers.length===0) return 0;
